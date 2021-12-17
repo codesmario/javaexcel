a54770e1bd9caf71c5ff579b29d44ae38268d7fb
ogo <mario.sanchez@udb.edu.sv>
2021-12-17 08:22:00 -0600

    Se anexan más columnas al documento utilizando datos de una lista... siguiente prueba datos de un json

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,15 +7,26 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
 
     public  static void main(String[] args) throws IOException {
         HSSFWorkbook workbook = new HSSFWorkbook();
         HSSFSheet sheet = workbook.createSheet("Mi 1ra hoja");
+        // Descripción, Monto, Tipo (Ingreso o Gasto), Categoría (Comida, Teléfono, Electricidad, Agua, Renta, o lo que se te ocurra).
+        ArrayList<String> columns = new ArrayList<>(Arrays.asList("No","Description","Amount","Category","Date"));
+
+
         HSSFRow row = sheet.createRow(0);
-        HSSFCell cell = row.createCell(0);
-        cell.setCellValue("Holaaaaa mundux");
+
+        for (int i = 0 ; i < columns.size() ; i++){
+            HSSFCell cell = row.createCell(i);
+            cell.setCellValue(columns.get(i));
+        }
+
+
 
         OutputStream out = new FileOutputStream("src/main/resources/SimpleExcel.xls");
         workbook.write(out);
