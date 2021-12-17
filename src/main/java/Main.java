dd2b7e3959cfcc3d666f1ffab7d4b66a3b7e3147
ogo <mario.sanchez@udb.edu.sv>
2021-12-17 13:15:54 -0600

    no estable

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class Main {
 
@@ -17,15 +18,16 @@ public class Main {
         HSSFSheet sheet = workbook.createSheet("Mi 1ra hoja");
         // Descripción, Monto, Tipo (Ingreso o Gasto), Categoría (Comida, Teléfono, Electricidad, Agua, Renta, o lo que se te ocurra).
         ArrayList<String> columns = new ArrayList<>(Arrays.asList("No","Description","Amount","Category","Date"));
-
-
+        /*
+            HashMap<String,String,String,String,String> datas = new HashMap<String,String,String,String,String>();
+            datas.put("1","Salud","5.00","Gasto","2021-12-17");
+        */
         HSSFRow row = sheet.createRow(0);
 
         for (int i = 0 ; i < columns.size() ; i++){
             HSSFCell cell = row.createCell(i);
             cell.setCellValue(columns.get(i));
         }
-
 
 
         OutputStream out = new FileOutputStream("src/main/resources/SimpleExcel.xls");
